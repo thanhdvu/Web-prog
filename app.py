7c2d97bc87c16b5e408175b8b58bb9ea0b18027f
Mincai Wang <mincaiw@yonsei.com>
2025-05-09 16:30:02 +0900

    ko/index_ko

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
-from flask import Flask
-from flask_cors import CORS
 from flask import Flask, render_template
+from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route('/')
 def home():
-    return render_template('index_ch.html')
+    return render_template('ko/index_ko.html')
 
 # Chinese page
 @app.route('/ch')
