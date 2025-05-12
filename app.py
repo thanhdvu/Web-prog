08c84a21c8014d3cb0172f40b42377b8401b84ea
thanhdvu <thuyanhdv@gmail.com>
2025-05-12 19:35:32 +0900

    conn data

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template
 from flask_cors import CORS
 
-app = Flask(__name__)
+app = Flask(_name_)
 CORS(app)
+
+from auth import auth_bp
+app.register_blueprint(auth_bp, url_prefix='/auth')
 
 @app.route('/')
 def home():
