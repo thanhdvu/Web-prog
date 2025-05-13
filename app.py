ccce93862fe94f5e28b67629b81eeb20da5dbe3a
Mincai Wang <mincaiw@yonsei.com>
2025-05-13 13:31:19 +0900

    .

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template
 from flask_cors import CORS
+from auth import auth_bp
 
 app = Flask(__name__)
 CORS(app)
 
-from auth import auth_bp
 app.register_blueprint(auth_bp, url_prefix='/auth')
 
 @app.route('/')
